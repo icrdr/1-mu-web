65f0c37ee9bf9aaf9ebdcf75b122c2341229ab3b
icrdr <icrdr2010@gmail.com>
2019-07-04 09:49:54 +0800

    config

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-export const DOMAIN_URL = 'http://localhost:5000'
-export const WX_APPID = 'wx9c88c3320f959b7c'
-export const  REDIRECT_URI = 'http://localhost:3000'
-export const  IMG_FORMAT = ['png','jpg','gif','jpeg','psd']
+var DOMAIN_URL = 'http://localhost:5000'
+var WX_APPID = 'wx9c88c3320f959b7c'
+var REDIRECT_URI = 'http://localhost:3000'
+var IMG_FORMAT = ['png','jpg','gif','jpeg','psd']
