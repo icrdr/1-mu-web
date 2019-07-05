7b5d0654f4ce2d4c18867b403baf94f1739f1e07
icrdr <icrdr2010@gmail.com>
2019-07-05 15:46:17 +0800

    1

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
   let wx_qrcode_url = ''
   if(isWx()){
     console.log(WX_GZ_APPID)
-    wx_qrcode_url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${WX_GZ_APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
+    wx_qrcode_url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${WX_GZ_APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
   }else{
     console.log(WX_KF_APPID)
     wx_qrcode_url = `https://open.weixin.qq.com/connect/qrconnect?appid=${WX_KF_APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect`
