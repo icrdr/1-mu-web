0a82336d8a68e5eec82739fcc7d5add6218ff2b7
icrdr <icrdr2010@gmail.com>
2019-07-08 11:33:35 +0800

    1

--- src/pages/FilePage.jsx
+++ src/pages/FilePage.jsx
@@ -4,7 +4,7 @@ import FilesList from '../components/FilesList'
 import { Upload, message, Button, Icon, Card, Row, Col } from 'antd';
 import { hasToken } from '../utility'
 import LoginQrcode from '../components/LoginQrcode'
-
+const { Dragger } = Upload;
 const DOMAIN_URL = window.DOMAIN_URL
 
 export default function MainPage(props) {
@@ -24,7 +24,7 @@ export default function MainPage(props) {
     name: 'file',
     action: `${DOMAIN_URL}/api/file`,
     withCredentials: true,
-    showUploadList: false,
+    multiple: true,
     onChange(info) {
       if (info.file.status !== 'uploading') {
         console.log(info.file);
@@ -46,11 +46,16 @@ export default function MainPage(props) {
     <Row type="flex" justify="space-around" align="middle">
       <Col xs={24} md={20}>
         <Card>
-          <Upload {...args}>
-            <Button>
-              <Icon type="upload" /> Click to Upload
-            </Button>
-          </Upload>
+          <Dragger  {...args}>
+              <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                band files
+              </p>
+          </Dragger>
           <FilesList update={update} />
         </Card>
       </Col>
