2d1891f0fb265ba996c43b5cf68bf0634b2061a4
icrdr <icrdr2010@gmail.com>
2019-07-08 08:16:59 +0800

    1

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import useInterval from '../hooks/useInterval'
 
 const REDIRECT_URI = window.REDIRECT_URI
-const WX_KF_APPID = window.WX_KF_APPID
+// const WX_KF_APPID = window.WX_KF_APPID
 const WX_GZ_APPID = window.WX_GZ_APPID
 const DOMAIN_URL = window.DOMAIN_URL
 
@@ -13,17 +13,12 @@ const WX_QRCODE_TRY = window.WX_QRCODE_TRY
 const WX_QRCODE_DELAY = window.WX_QRCODE_DELAY
 
 export default function LoginPage() {
-  let wx_qrcode_url = ''
-  if (isWx()) {
-    wx_qrcode_url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${WX_GZ_APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
-  } else {
-    wx_qrcode_url = `https://open.weixin.qq.com/connect/qrconnect?appid=${WX_KF_APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect`
-  }
+  let wx_qrcode_url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${WX_GZ_APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
 
   const [qrcode, setQrcode] = useState('');
   const [scene_str, setScene_str] = useState('');
   const [count, setCount] = useState(1);
-  const [isChecking, setIsChecking] = useState(true);
+  const [isChecking, setIsChecking] = useState(false);
 
   useInterval(() => {
     fetchCheck()
@@ -35,14 +30,16 @@ export default function LoginPage() {
 
   useEffect(() => {
     let url = DOMAIN_URL + '/api/wechat/qrcode'
-    axios.get(url).then(res => {
-      console.log(res.data)
-      setQrcode(`https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=${res.data.ticket}`)
-      setScene_str(res.data.scene_str)
-    }).catch(err => {
-      if (err.response) console.log(err.response.data)
-    })
-  }, []);
+    if (isChecking) {
+      axios.get(url).then(res => {
+        console.log(res.data)
+        setQrcode(`https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=${res.data.ticket}`)
+        setScene_str(res.data.scene_str)
+      }).catch(err => {
+        if (err.response) console.log(err.response.data)
+      })
+    }
+  }, [isChecking]);
 
   const fetchCheck = () => {
     let url = DOMAIN_URL + '/api/wechat/check'
@@ -55,21 +52,27 @@ export default function LoginPage() {
       if (err.response) console.log(err.response.data)
     })
   }
+  let loginRender
+
+  if (isWx()){
+    loginRender = <Button type="primary" href={wx_qrcode_url} block>微信登陆</Button>
+  }else{
+    if (isChecking){
+      loginRender = <img width='100%' src={qrcode} alt='qrcode' />
+    }else{
+      loginRender = <Button onClick={() => setIsChecking(true)} block>微信登陆</Button>
+    }
+  }
 
   return (
-    <Row type="flex" justify="center" style={{ height: '1000px', background: '#eee' }}>
-      {isWx()}
-      <Col xs={22} md={10} style={{ height: 'auto' }}>
-        <Card>
-          <img width='100%' src={qrcode} alt='qrcode' />
-          <Button type="primary" href={wx_qrcode_url} block>
-            微信登陆
-          </Button>
-          <Button onClick={() => setIsChecking(true)} block>
-            测试是否扫描
-          </Button>
-        </Card>
-      </Col>
-    </Row>
+    <div>
+      <Row type="flex" justify="center" style={{ height: '1000px', background: '#eee' }}>
+        <Col xs={22} sm={14} md={6} style={{ height: 'auto' }}>
+          <Card>
+            {loginRender}
+          </Card>
+        </Col>
+      </Row>
+    </div>
   )
 }
