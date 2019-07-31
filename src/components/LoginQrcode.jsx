3bc48f46cdc194c200227bd865c02380b63bc970
icrdr <icrdr2010@gmail.com>
2019-07-31 15:49:19 +0800

    1

--- src/components/LoginQrcode.jsx
+++ src/components/LoginQrcode.jsx
@@ -69,7 +69,7 @@ function LoginQrcode({ location, history }) {
   }
   let loginRender
 
-  if (!isWx()) {
+  if (isWx()) {
     loginRender = <Button type="primary" href={wx_qrcode_url} block>微信登陆</Button>
   } else {
     if (isChecking) {
