39943bfb7246d642e237874a5a76dbc50b0fcc36
icrdr <icrdr2010@gmail.com>
2019-08-20 07:11:11 +0800

    1

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState,useContext } from 'react'
+import { Result, Icon, Button } from 'antd';
+import { meContext } from '../layouts/Web';
+import { fetchData } from '../utility'
 
-export default function Main() {
+export default function Main({history}) {
+  const [total, setTotal] = useState()
+  const { meData } = useContext(meContext);
+  useEffect(() => {
+    const path = '/projects'
+    const params = {
+      order: 'desc',
+      pre_page: 20,
+      status: 'progress,modify,delay,pending',
+      order_by: 'status',
+      creator_id: meData.id
+    }
+
+    fetchData(path, params).then(res => {
+      setTotal(res.data.total)
+    })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>
-      main
-    </div>
+    <Result
+    icon={<Icon type="bell" theme="twoTone" />}
+    title={total?`目前您有${total}个企划正在进行`:"嘿嘿嘿，您可以歇一歇了，没活儿"}
+    extra={<Button type="primary" onClick={()=>{history.push('/projects')}}>查看进度</Button>}
+  />
   )
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Result, Button } from 'antd';
 
-export default function NotFound() {
+export default function NotFound({history}) {
   return (
-    <h1>
-      404
-    </h1>
+    <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={<Button type="primary" onClick={()=>{history.push('/')}}>返回首页</Button>}
+    />
   )
 }
