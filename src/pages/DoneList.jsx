6d1926c0eef22fc64b748d4df6b8fd1d5b46694a
icrdr <icrdr2010@gmail.com>
2019-11-02 08:53:03 +0800

    1

--- src/pages/DoneList.jsx
+++ src/pages/DoneList.jsx
@@ -1,104 +1,126 @@
-import React, { useEffect, useState, useCallback } from 'react'
-import { Card, Input, Modal, Tag, Button, BackTop } from 'antd'
-import { fetchData, getPhase} from '../utility'
-import ImgCard from '../components/ImgCard'
-import queryString from 'query-string'
-import { useMediaQuery } from 'react-responsive'
+import React, { useEffect, useState, useCallback } from "react";
+import { Card, Input, Modal, Tag, Button, BackTop, Statistic } from "antd";
+import { fetchData, getPhase } from "../utility";
+import ImgCard from "../components/ImgCard";
+import queryString from "query-string";
+import { useMediaQuery } from "react-responsive";
 import StackGrid from "react-stack-grid";
-import useEvent from '../hooks/useEvent'
+import useEvent from "../hooks/useEvent";
 const { Search } = Input;
 export default function DoneList({ location, history }) {
-  const isSm = useMediaQuery({ query: '(max-width: 768px)' })
-  const [stackGrid, setStackGrid] = useState()
-  const [update, setUpdate] = useState(false)
-  const [page, setPage] = useState(1)
+  const isSm = useMediaQuery({ query: "(max-width: 768px)" });
+  const [stackGrid, setStackGrid] = useState();
+  const [update, setUpdate] = useState(false);
+  const [page, setPage] = useState(1);
   const [projectList, setProjectList] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [isEnd, setEnd] = useState(false);
-  const [lightBox, setLightBox] = useState()
+  const [lightBox, setLightBox] = useState();
+  const [total, seTtotal] = useState(0);
 
   useEffect(() => {
-    setLoading(true)
-    const path = '/projects'
+    setLoading(true);
+    const path = "/projects";
     const params = {
-      order: 'desc',
-      order_by: 'finish_date',
+      order: "desc",
+      order_by: "finish_date",
       pre_page: 12,
       page: page,
-      status: 'finish',
-      tags: '腾讯医典词条',
-    }
+      status: "finish",
+      tags: "腾讯医典词条"
+    };
 
-    const values = queryString.parse(location.search)
+    const values = queryString.parse(location.search);
 
     if (values.search) {
-      params.search = values.search
+      params.search = values.search;
     }
 
     fetchData(path, params).then(res => {
       setProjectList(prevState => {
-        return prevState.concat(res.data.projects)
-      })
+        return prevState.concat(res.data.projects);
+      });
+      seTtotal(res.data.total);
       if (res.data.projects.length > 0) {
-        setPage(prevState => { return prevState + 1 })
+        setPage(prevState => {
+          return prevState + 1;
+        });
       } else {
-        setEnd(true)
+        setEnd(true);
       }
       setTimeout(() => {
-        if (stackGrid) stackGrid.updateLayout()
-        setLoading(false)
+        if (stackGrid) stackGrid.updateLayout();
+        setLoading(false);
       }, 200);
-    })
+    });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [update, location]);
 
   const handleScroll = useCallback(() => {
-    if (document.documentElement.offsetHeight + document.documentElement.scrollTop < document.documentElement.scrollHeight - 100) return
+    if (
+      document.documentElement.offsetHeight +
+        document.documentElement.scrollTop <
+      document.documentElement.scrollHeight - 100
+    )
+      return;
     if (!isLoading && !isEnd) {
-      setUpdate(!update)
+      setUpdate(!update);
     }
     // eslint-disable-next-line
-  }, [isLoading])
+  }, [isLoading]);
 
-  useEvent('scroll', handleScroll)
+  useEvent("scroll", handleScroll);
 
   const onSearch = v => {
-    setProjectList([])
-    setPage(1)
-    const values = queryString.parse(location.search)
+    setProjectList([]);
+    setPage(1);
+    const values = queryString.parse(location.search);
     const params = queryString.stringify({ ...values, search: v, page: 1 });
-    history.push(`${location.pathname}?${params}`)
-  }
+    history.push(`${location.pathname}?${params}`);
+  };
 
   return (
     <>
-      {lightBox != null &&
+      {lightBox != null && (
         <Modal
           title={lightBox.title}
           centered
           visible={lightBox != null}
           onCancel={() => setLightBox()}
-          okButtonProps={{ className: 'd:n' }}
-          cancelButtonProps={{ className: 'd:n' }}
-          width={isSm ? '100%' : '60%'}
+          okButtonProps={{ className: "d:n" }}
+          cancelButtonProps={{ className: "d:n" }}
+          width={isSm ? "100%" : "60%"}
           bodyStyle={{
             padding: 0
           }}
-        >{getPhase(lightBox.stages[lightBox.stages.length-1]).upload_files.map((file, index) => { return <ImgCard key={index} file={file} /> })}
-          <div className='p:2'>
-            {lightBox.tags.map((tag, index) => <Tag key={index}>{tag.name}</Tag>)}
+        >
+          {getPhase(
+            lightBox.stages[lightBox.stages.length - 1]
+          ).upload_files.map((file, index) => {
+            return <ImgCard key={index} file={file} />;
+          })}
+          <div className="p:2">
+            {lightBox.tags.map((tag, index) => (
+              <Tag key={index}>{tag.name}</Tag>
+            ))}
           </div>
         </Modal>
-      }
+      )}
       <BackTop />
       <Card>
-        <div className='m-b:1' >
-          <Search placeholder="输入企划标题关键词" onSearch={onSearch} allowClear enterButton />
+        <Statistic className="m-b:1" title="已完成总数" value={total} />
+        <div className="m-b:1">
+          <Search
+            placeholder="输入企划标题关键词"
+            onSearch={onSearch}
+            allowClear
+            enterButton
+          />
         </div>
         <StackGrid
-          className='m-b:1'
-          columnWidth={isSm ? '100%' : '33.33%'}
+          className="m-b:1"
+          columnWidth={isSm ? "100%" : "33.33%"}
           monitorImagesLoaded={true}
           gridRef={grid => setStackGrid(grid)}
           duration={180}
@@ -106,19 +128,33 @@ export default function DoneList({ location, history }) {
           gutterHeight={12}
         >
           {projectList.map((project, index) => {
-            const item = getPhase(project.stages[project.stages.length-1]).upload_files[0]
-            return <Card key={index} onClick={() => setLightBox(project)} cover={<ImgCard file={item} />}>
-              {project.title}
-            </Card>
+            const item = getPhase(project.stages[project.stages.length - 1])
+              .upload_files[0];
+            return (
+              <Card
+                key={index}
+                onClick={() => setLightBox(project)}
+                cover={<ImgCard file={item} />}
+              >
+                {project.title}
+              </Card>
+            );
           })}
         </StackGrid>
         {!isEnd ? (
-          <Button type="primary" loading={isLoading} disabled={isEnd} onClick={() => setUpdate(!update)} block>
-            {isLoading ? '加载中' : '点击加载'}
+          <Button
+            type="primary"
+            loading={isLoading}
+            disabled={isEnd}
+            onClick={() => setUpdate(!update)}
+            block
+          >
+            {isLoading ? "加载中" : "点击加载"}
           </Button>
-        ) : (<div className='t-a:c'>没有更多内容啦...</div>)}
+        ) : (
+          <div className="t-a:c">没有更多内容啦...</div>
+        )}
       </Card>
     </>
-  )
+  );
 }
-
