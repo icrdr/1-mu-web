b5825e9a70e02592019a122835171283879bb275
manan <icrdr2010@outlook.com>
2020-11-17 10:06:59 +0800

    add baidu sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,100 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 // pages
-import Main from './pages/Main'
-import Me from './pages/Me'
-import ProjectList from './pages/ProjectList'
-import Project from './pages/Project'
-import AProjectList from './pages/AProjectList'
-import AUserList from './pages/AUserList'
-import AProjectPost from './pages/AProjectPost'
-import AUser from './pages/AUser'
-import AGroupList from './pages/AGroupList'
-import AGroup from './pages/AGroup'
-import GroupList from './pages/GroupList'
-import Group from './pages/Group'
-import User from './pages/User'
-import FileList from './pages/FileList'
-import NotFound from './pages/NotFound'
-import SampleList from './pages/SampleList'
-import AGroupAdd from './pages/AGroupAdd'
-import DoneList from './pages/DoneList'
-import Dashboard from './pages/Dashboard'
-import DraftList from './pages/DraftList'
-import AProjectRecycle from './pages/AProjectRecycle'
+import Main from "./pages/Main";
+import Me from "./pages/Me";
+import ProjectList from "./pages/ProjectList";
+import Project from "./pages/Project";
+import AProjectList from "./pages/AProjectList";
+import AUserList from "./pages/AUserList";
+import AProjectPost from "./pages/AProjectPost";
+import AUser from "./pages/AUser";
+import AGroupList from "./pages/AGroupList";
+import AGroup from "./pages/AGroup";
+import GroupList from "./pages/GroupList";
+import Group from "./pages/Group";
+import User from "./pages/User";
+import FileList from "./pages/FileList";
+import NotFound from "./pages/NotFound";
+import SampleList from "./pages/SampleList";
+import SampleList2 from "./pages/SampleList2";
+import AGroupAdd from "./pages/AGroupAdd";
+import DoneList from "./pages/DoneList";
+import Dashboard from "./pages/Dashboard";
+import DraftList from "./pages/DraftList";
+import AProjectRecycle from "./pages/AProjectRecycle";
 // layouts
-import Web from './layouts/Web'
-import Admin from './layouts/Admin'
-import useWxLogin from './hooks/useWxLogin'
+import Web from "./layouts/Web";
+import Admin from "./layouts/Admin";
+import useWxLogin from "./hooks/useWxLogin";
 import { withRouter } from "react-router";
-import LoginQrcode from './components/LoginQrcode'
-import useLogin from './hooks/useLogin'
-import Loading from './components/Loading'
-import Maintenance from './components/Maintenance'
-import { useMediaQuery } from 'react-responsive'
+import LoginQrcode from "./components/LoginQrcode";
+import useLogin from "./hooks/useLogin";
+import Loading from "./components/Loading";
+import Maintenance from "./components/Maintenance";
+import { useMediaQuery } from "react-responsive";
 //css
-import './App.css';
-import Aoption from './pages/Aoption';
+import "./App.css";
+import Aoption from "./pages/Aoption";
 export const globalContext = React.createContext();
 function App({ location }) {
-  
-  const wxLoginState = useWxLogin(location)
-  const { meData, status } = useLogin()
-  const isSm = useMediaQuery({ query: '(max-width: 768px)' })
+  const wxLoginState = useWxLogin(location);
+  const { meData, status } = useLogin();
+  const isSm = useMediaQuery({ query: "(max-width: 768px)" });
   switch (wxLoginState) {
-    case 'pending':
+    case "pending":
       return <div>loading...</div>;
-    case 'error':
+    case "error":
       return <div>error!</div>;
     default:
       break;
   }
 
   switch (status) {
-    case 'pending':
-      return <Loading />
-    case 'no':
-      return <LoginQrcode />
-    case 'error':
-      return <Maintenance />
+    case "pending":
+      return <Loading />;
+    case "no":
+      return <LoginQrcode />;
+    case "error":
+      return <Maintenance />;
     default:
-      break
+      break;
   }
 
   return (
     <globalContext.Provider value={{ meData, isSm }}>
-    <Switch>
-      <Web exact path="/" component={Main} />
-      <Web path="/me" component={Me} />
-      <Web exact path="/samples" component={SampleList} />
-      <Web exact path="/dones" component={DoneList} />
-      <Web exact path="/projects" component={ProjectList} />
-      <Web exact path="/resources" component={FileList} />
-      <Web exact path="/drafts" component={DraftList} />
-      <Web exact path="/dashboard" component={Dashboard} />
-      <Web path="/projects/:project_id(\d+)" component={Project} />
-      <Web path="/users/:user_id(\d+)" component={User} />
-      <Web exact path="/groups" component={GroupList} />
-      <Web path="/groups/:group_id(\d+)" component={Group} />
-      <Admin exact path="/admin/projects" component={AProjectList} />
-      <Admin exact path="/admin/recycle" component={AProjectRecycle} />
-      <Admin path="/admin/projects/:project_id(\d+)" component={Project} props={{isAdmin:true}}/>
-      <Admin path="/admin/projects/post" component={AProjectPost} />
-      <Admin exact path="/admin/users" component={AUserList} />
-      <Admin path="/admin/option" component={Aoption} />
-      <Admin path="/admin/users/:user_id(\d+)" component={AUser} />
-      <Admin exact path="/admin/groups" component={AGroupList} />
-      <Admin path="/admin/groups/:group_id(\d+)" component={AGroup} />
-      <Admin path="/admin/groups/add" component={AGroupAdd} />
-      <Route component={NotFound} />
-    </Switch>
+      <Switch>
+        <Web exact path="/" component={Main} />
+        <Web path="/me" component={Me} />
+        <Web exact path="/samples" component={SampleList} />
+        <Web exact path="/bdsamples" component={SampleList2} />
+        <Web exact path="/dones" component={DoneList} />
+        <Web exact path="/projects" component={ProjectList} />
+        <Web exact path="/resources" component={FileList} />
+        <Web exact path="/drafts" component={DraftList} />
+        <Web exact path="/dashboard" component={Dashboard} />
+        <Web path="/projects/:project_id(\d+)" component={Project} />
+        <Web path="/users/:user_id(\d+)" component={User} />
+        <Web exact path="/groups" component={GroupList} />
+        <Web path="/groups/:group_id(\d+)" component={Group} />
+        <Admin exact path="/admin/projects" component={AProjectList} />
+        <Admin exact path="/admin/recycle" component={AProjectRecycle} />
+        <Admin
+          path="/admin/projects/:project_id(\d+)"
+          component={Project}
+          props={{ isAdmin: true }}
+        />
+        <Admin path="/admin/projects/post" component={AProjectPost} />
+        <Admin exact path="/admin/users" component={AUserList} />
+        <Admin path="/admin/option" component={Aoption} />
+        <Admin path="/admin/users/:user_id(\d+)" component={AUser} />
+        <Admin exact path="/admin/groups" component={AGroupList} />
+        <Admin path="/admin/groups/:group_id(\d+)" component={AGroup} />
+        <Admin path="/admin/groups/add" component={AGroupAdd} />
+        <Route component={NotFound} />
+      </Switch>
     </globalContext.Provider>
-  )
+  );
 }
-export default withRouter(App)
+export default withRouter(App);
---
+++ src/pages/SampleList2.jsx
@@ -0,0 +1,194 @@
+import React, { useEffect, useState, useCallback } from "react";
+import { Card, Input, Modal, Tag, Button, Icon, message, BackTop } from "antd";
+import { fetchData, getPhase } from "../utility";
+import ImgCard from "../components/ImgCard";
+import queryString from "query-string";
+import { useMediaQuery } from "react-responsive";
+import StackGrid from "react-stack-grid";
+import useEvent from "../hooks/useEvent";
+const { Search } = Input;
+export default function SampleList({ location, history }) {
+  const isSm = useMediaQuery({ query: "(max-width: 768px)" });
+  const [stackGrid, setStackGrid] = useState();
+  const [update, setUpdate] = useState(false);
+  const [page, setPage] = useState(1);
+  const [projectList, setProjectList] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [lightBox, setLightBox] = useState();
+  const [isZipping, setZipping] = useState(false);
+  const [isEnd, setEnd] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const path = "/projects";
+    const params = {
+      order: "desc",
+      pre_page: 12,
+      page: page,
+      status: "finish",
+      tags: "百度样图",
+    };
+
+    const values = queryString.parse(location.search);
+
+    if (values.search) {
+      params.search = values.search;
+    }
+
+    fetchData(path, params).then((res) => {
+      setProjectList((prevState) => {
+        return prevState.concat(res.data.projects);
+      });
+      if (res.data.projects.length > 0) {
+        setPage((prevState) => {
+          return prevState + 1;
+        });
+      } else {
+        setEnd(true);
+      }
+      setTimeout(() => {
+        if (stackGrid) stackGrid.updateLayout();
+        setLoading(false);
+      }, 200);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [update, location]);
+
+  const handleScroll = useCallback(() => {
+    if (
+      document.documentElement.offsetHeight +
+        document.documentElement.scrollTop <
+      document.documentElement.scrollHeight - 100
+    )
+      return;
+    if (!isLoading && !isEnd) {
+      setUpdate(!update);
+    }
+    // eslint-disable-next-line
+  }, [isLoading]);
+
+  useEvent("scroll", handleScroll);
+
+  const onSearch = (v) => {
+    setProjectList([]);
+    setPage(1);
+    const values = queryString.parse(location.search);
+    const params = queryString.stringify({ ...values, search: v, page: 1 });
+    history.push(`${location.pathname}?${params}`);
+  };
+
+  const handleDownload = (files) => {
+    setZipping(true);
+    const file_id = [];
+    for (const file of files) {
+      file_id.push(file.id);
+    }
+    const path = "/download/files";
+    const params = {
+      file_id: file_id.join(","),
+    };
+    const hide = message.loading("压缩文件中...", 0);
+    fetchData(path, params)
+      .then((res) => {
+        hide();
+        window.location.href = res.data.download_url;
+      })
+      .finally(() => {
+        setZipping(false);
+      });
+  };
+
+  return (
+    <>
+      {lightBox != null && (
+        <Modal
+          title={lightBox.title}
+          centered
+          visible={lightBox != null}
+          onCancel={() => setLightBox()}
+          okButtonProps={{ className: "d:n" }}
+          cancelButtonProps={{ className: "d:n" }}
+          width={isSm ? "100%" : "60%"}
+          bodyStyle={{
+            padding: 0,
+          }}
+        >
+          {getPhase(
+            lightBox.stages[lightBox.stages.length - 1]
+          ).upload_files.map((file, index) => {
+            return <ImgCard key={index} file={file} />;
+          })}
+          <div className="p:2">
+            {lightBox.tags.map((tag, index) => (
+              <Tag key={index}>{tag.name}</Tag>
+            ))}
+          </div>
+        </Modal>
+      )}
+      <BackTop />
+      <Card>
+        <div className="m-b:1">
+          <Search
+            placeholder="输入企划标题关键词"
+            onSearch={onSearch}
+            allowClear
+            enterButton
+          />
+        </div>
+        <StackGrid
+          className="m-b:1"
+          columnWidth={isSm ? "100%" : "33.33%"}
+          monitorImagesLoaded={true}
+          gridRef={(grid) => setStackGrid(grid)}
+          duration={180}
+          gutterWidth={12}
+          gutterHeight={12}
+        >
+          {projectList.map((project, index) => {
+            const item = getPhase(project.stages[project.stages.length - 1])
+              .upload_files[0];
+            return (
+              <Card
+                key={index}
+                cover={
+                  <div onClick={() => setLightBox(project)}>
+                    <ImgCard file={item} />
+                  </div>
+                }
+              >
+                <Button
+                  type="link"
+                  size="small"
+                  disabled={isZipping}
+                  onClick={() =>
+                    handleDownload(
+                      getPhase(project.stages[project.stages.length - 1])
+                        .upload_files
+                    )
+                  }
+                >
+                  <Icon type="download" />
+                  {project.title}
+                </Button>
+              </Card>
+            );
+          })}
+        </StackGrid>
+        {!isEnd ? (
+          <Button
+            type="primary"
+            loading={isLoading}
+            disabled={isEnd}
+            onClick={() => setUpdate(!update)}
+            block
+          >
+            {isLoading ? "加载中" : "点击加载"}
+          </Button>
+        ) : (
+          <div className="t-a:c">没有更多内容啦...</div>
+        )}
+      </Card>
+    </>
+  );
+}
